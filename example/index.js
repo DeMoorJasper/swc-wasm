caad4fa3aa5340126d5bba9767574f4bdf8ba933
Jasper De Moor <jasperdemoor@gmail.com>
2021-06-04 21:23:02 +0200

    more benchmarking

--- example/index.js
+++ example/index.js
@@ -1,13 +1,20 @@
+import * as Babel from "@babel/standalone";
 import initSWC, { transformSync } from "../lib/wasm";
 
-const ITERATIONS = 250;
+const ITERATIONS = 10;
 
-async function run() {
+let code = "";
+async function fetchCode(url) {
+  let fetchResponse = await fetch(url);
+  code = await fetchResponse.text();
+}
+
+async function runSWC() {
   await initSWC();
   const startTime = Date.now();
   let result;
   for (let i = 0; i < ITERATIONS; i++) {
-    result = transformSync("export default function test() {}", {
+    result = transformSync(code, {
       filename: "input.js",
       sourceMaps: false,
       isModule: true,
@@ -26,13 +33,50 @@ async function run() {
       },
     });
   }
-  console.log(result);
+  console.log(result.code.length);
   let duration = Date.now() - startTime;
   console.log(
-    `Took ${duration}ms in total, ${(duration / ITERATIONS).toFixed(
+    `SWC Took ${duration}ms in total, ${(duration / ITERATIONS).toFixed(
       2
     )}ms per iteration`
   );
+}
+
+async function runBabel() {
+  const startTime = Date.now();
+  let result;
+  for (let i = 0; i < ITERATIONS; i++) {
+    result = Babel.transform(code, {
+      presets: [["env", {}]],
+      plugins: ["transform-react-jsx"],
+    });
+  }
+  console.log(result.code.length);
+  let duration = Date.now() - startTime;
+  console.log(
+    `Babel Took ${duration}ms in total, ${(duration / ITERATIONS).toFixed(
+      2
+    )}ms per iteration`
+  );
+}
+
+async function run() {
+  await fetchCode();
+  console.log(code);
+
+  code = "export const App = () => <div>Test</div>;";
+  await runSWC();
+  await runBabel();
+
+  await fetchCode("https://unpkg.com/react@17.0.2/umd/react.development.js");
+  await runSWC();
+  await runBabel();
+
+  await fetchCode(
+    "https://unpkg.com/react-dom@17.0.2/umd/react-dom.development.js"
+  );
+  await runSWC();
+  await runBabel();
 }
 
 run().catch(console.error);
